a26d10f2cd39b87750a4f18ae273cc3d182071bc
zjtie <51729207+zjtie@users.noreply.github.com>
2025-08-17 22:05:28 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,7 +2,132 @@ const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
-    }
+    },
+    dyttzy: {
+        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
+        name: '电影天堂资源',
+        detail: 'http://caiji.dyttzyapi.com', 
+    },
+    ruyi: {
+        api: 'https://cj.rycjapi.com/api.php/provide/vod',
+        name: '如意资源',
+    },
+    bfzy: {
+        api: 'https://bfzyapi.com/api.php/provide/vod',
+        name: '暴风资源',
+    },
+    tyyszy: {
+        api: 'https://tyyszy.com/api.php/provide/vod',
+        name: '天涯资源',
+    },
+    ffzy: {
+        api: 'http://ffzy5.tv/api.php/provide/vod',
+        name: '非凡影视',
+        detail: 'http://ffzy5.tv', 
+    },
+    heimuer: {
+        api: 'https://json.heimuer.xyz/api.php/provide/vod',
+        name: '黑木耳',
+        detail: 'https://heimuer.tv', 
+    },
+    zy360: {
+        api: 'https://360zy.com/api.php/provide/vod',
+        name: '360资源',
+    },
+    iqiyi: {
+        api: 'https://www.iqiyizyapi.com/api.php/provide/vod',
+        name: 'iqiyi资源',
+    },
+    wolong: {
+        api: 'https://wolongzyw.com/api.php/provide/vod',
+        name: '卧龙资源',
+    }, 
+    hwba: {
+        api: 'https://cjhwba.com/api.php/provide/vod',
+        name: '华为吧资源',
+    },
+    jisu: {
+        api: 'https://jszyapi.com/api.php/provide/vod',
+        name: '极速资源',
+        detail: 'https://jszyapi.com', 
+    },
+    dbzy: {
+        api: 'https://dbzy.com/api.php/provide/vod',
+        name: '豆瓣资源',
+    },
+    mozhua: {
+        api: 'https://mozhuazy.com/api.php/provide/vod',
+        name: '魔爪资源',
+    },
+    mdzy: {
+        api: 'https://www.mdzyapi.com/api.php/provide/vod',
+        name: '魔都资源',
+    },
+    zuid: {
+        api: 'https://api.zuidapi.com/api.php/provide/vod',
+        name: '最大资源'
+    },
+    yinghua: {
+        api: 'https://m3u8.apiyhzy.com/api.php/provide/vod',
+        name: '樱花资源'
+    },
+    baidu: {
+        api: 'https://api.apibdzy.com/api.php/provide/vod',
+        name: '百度云资源'
+    },
+    wujin: {
+        api: 'https://api.wujinapi.me/api.php/provide/vod',
+        name: '无尽资源'
+    },
+    wwzy: {
+        api: 'https://wwzy.tv/api.php/provide/vod',
+        name: '旺旺短剧'
+    },
+    ikun: {
+        api: 'https://ikunzyapi.com/api.php/provide/vod',
+        name: 'iKun资源'
+    },
+ckzy: {
+    api: 'https://www.ckzy1.com',
+    name: 'CK资源',
+    adult: true
+},
+jkun: {
+    api: 'https://jkunzyapi.com',
+    name: 'jkun资源',
+    adult: true
+},
+bwzy: {
+    api: 'https://api.bwzym3u8.com',
+    name: '百万资源',
+    adult: true
+},
+souav: {
+    api: 'https://api.souavzy.vip',
+    name: 'souav资源',
+    adult: true
+},
+r155: {
+    api: 'https://155api.com',
+    name: '155资源',
+    adult: true
+},
+lsb: {
+    api: 'https://apilsbzy1.com',
+    name: 'lsb资源',
+    adult: true
+},
+huangcang: {
+    api: 'https://hsckzy.vip',
+    name: '黄色仓库',
+    adult: true,
+    detail: 'https://hsckzy.vip'
+},
+yutu: {
+    api: 'https://yutuzy10.com',
+    name: '玉兔资源',
+    adult: true
+}    
 };
 
 // 调用全局方法合并
